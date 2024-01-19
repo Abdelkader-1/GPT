3a81a8554f1c96eb66dabfc93896bcd7a9fdfe07
Abdelkader <abdelkadertalaat65@gmail.com>
2024-01-19 17:25:24 +0200

    passwordChange

--- Db_authentication.py
+++ Db_authentication.py
@@ -42,7 +42,7 @@ class Auth:
         # Encode @ symbols for compatibility
         user = self.email.replace("@", "%40")
         password = self.password.replace("@", "%40")
-        db_string = f'mssql+pyodbc://{user}:{password}@{self.server}/{self.database}?driver={self.driver}&Trusted_Connection=no&Authentication=ActiveDirectoryInteractive'
+        db_string = f'mssql+pyodbc://{user}:{"a2ff52c3-236b-4ef4-9f31-e5bf5fd41a97"}@{self.server}/{self.database}?driver={self.driver}&Trusted_Connection=no&Authentication=ActiveDirectoryInteractive'
         #db_string = f'mssql+pyodbc://{user}:{password}@{self.server}/{self.database}?driver={self.driver}&Trusted_Connection=no'
         engine = sa.create_engine(db_string, echo=True, connect_args={'autocommit': True}, fast_executemany=True,pool_pre_ping=True)
         return engine
