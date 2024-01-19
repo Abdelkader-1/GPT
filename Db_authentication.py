cf20b13f839fac5e07293096fa8ab6500a94b0fa
Abdelkader <abdelkadertalaat65@gmail.com>
2024-01-19 16:49:03 +0200

    pool_pre_ping=True

--- Db_authentication.py
+++ Db_authentication.py
@@ -44,7 +44,7 @@ class Auth:
         password = self.password.replace("@", "%40")
         db_string = f'mssql+pyodbc://{user}:{password}@{self.server}/{self.database}?driver={self.driver}&Trusted_Connection=no&Authentication=ActiveDirectoryInteractive'
         #db_string = f'mssql+pyodbc://{user}:{password}@{self.server}/{self.database}?driver={self.driver}&Trusted_Connection=no'
-        engine = sa.create_engine(db_string, echo=True, connect_args={'autocommit': True}, fast_executemany=True)
+        engine = sa.create_engine(db_string, echo=True, connect_args={'autocommit': True}, fast_executemany=True,pool_pre_ping=True)
         return engine
     def schema_describtion(self):
         sql_query = '''
