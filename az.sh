139760a0f83651020580abb14519bb0c7c84a938
Abdelkader <abdelkadertalaat65@gmail.com>
2024-01-22 12:06:43 +0200

    sh

--- az.sh
+++ az.sh
@@ -1,11 +1,1 @@
-az ad sp create-for-rbac --name "abianalysis" --sdk-auth --role contributor --scopes /subscriptions/b47aa3c4-25c7-4b90-a5cd-87a81216f25f/resourceGroups/ABItest/providers/Microsoft.Web/sites/ABItest
-
-
-az ad sp create-for-rbac --name "abianalysis" --role contributor \
-                            --scopes /subscriptions/b47aa3c4-25c7-4b90-a5cd-87a81216f25f/resourceGroups/ABItest \
-                            --sdk-auth
-
-
-                            az ad sp create-for-rbac --name "abiassis" --role contributor \
-                            --scopes /subscriptions/b47aa3c4-25c7-4b90-a5cd-87a81216f25f/resourceGroups/"abi2" \
-                            --sdk-auth
+export LD_LIBRARY_PATH=/GPT\/to/odbc/driver:$LD_LIBRARY_PATH
